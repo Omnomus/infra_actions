897e519b6a82c9ab3554bd7c93eebe98750d1fbd
Omnomus <bka@inbox.ru>
2021-09-19 18:53:32 +0300

    added tests to workflow

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! И я все смогу!')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
